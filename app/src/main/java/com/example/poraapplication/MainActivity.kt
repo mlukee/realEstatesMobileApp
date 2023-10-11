cb1a58cf90e6b06364203ce84b9ab1d22a5e0f64
Matic Lukežič <maticlukezic007@gmail.com>
2023-10-11 22:34:32 +0200

    Add input elements and buttons and their functionality

--- app/src/main/java/com/example/poraapplication/MainActivity.kt
+++ app/src/main/java/com/example/poraapplication/MainActivity.kt
@@ -1,21 +1,66 @@
 package com.example.poraapplication
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.example.lib.RealEstate
 import com.example.poraapplication.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var realEstates = mutableListOf<RealEstate>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
-    fun onButttonInfoClick(view: View){
-        Log.d(MainActivity::class.simpleName, 5.toString())
-        //MainActivity::class.simpleName lahko zamenjas z svojo znacko da lahko lazje isces v Logcat
+    fun onButtonInfoClick(view: View){
+        Log.i(MainActivity::class.simpleName, "Number of elements in list: ${realEstates.size}")
+    }
+
+    fun onButtonAddClick(view: View) {
+        val propertyType = binding.editTextPropertyType.text.toString()
+        val areaText = binding.editTextArea.text.toString()
+        val priceText = binding.editTextPrice.text.toString()
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        try {
+            if (propertyType.isEmpty() || areaText.isEmpty() || priceText.isEmpty()) {
+                throw IllegalArgumentException("All fields are required")
+            }
+
+            val area = areaText.toDouble()
+            val price = priceText.toDouble()
+
+            println("$propertyType\t$area\t$price")
+            val estate = RealEstate(propertyType, area, price)
+
+
+            if(realEstates.add(estate)) {
+                alertDialogBuilder.setMessage("Successfully added: ${estate.propertyType}, ${estate.area}, ${estate.price}")
+                    .setPositiveButton("OK") { dialogInterface,i -> dialogInterface.dismiss()} .show()
+                binding.editTextPropertyType.text.clear()
+                binding.editTextArea.text.clear()
+                binding.editTextPrice.text.clear()
+            }
+
+
+        } catch (e: IllegalArgumentException) {
+            alertDialogBuilder.setMessage(e.message).setPositiveButton("OK"
+            ) { dialogInterface, i -> dialogInterface.dismiss() }
+            alertDialogBuilder.show()
+        }
+    }
+
+    fun onButtonExitClick(view: View){
+        MainActivity().finish()
+        exitProcess(0)
     }
 }
